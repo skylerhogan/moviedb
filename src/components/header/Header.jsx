92b585a0fd954a60f8e41aaced0e2401843216f8
Skyler Hogan <skylerhogan@skylers-imac.attlocal.net>
2021-10-17 10:25:00 -0500

    finished header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 import { Link, useLocation } from "react-router-dom";
 
 import "./header.scss";
 
 import logo from "../../assets/film.svg";
+import { EffectFlip } from "swiper";
 
 const headerNav = [
   {
@@ -26,6 +27,20 @@ export const Header = () => {
   const headerRef = useRef(null);
 
   const active = headerNav.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        headerRef.current.classList.add('shrink');
+      } else {
+        headerRef.current.classList.remove('shrink');
+      }
+    }
+    window.addEventListener('scroll', shrinkHeader);
+    return () => {
+      window.removeEventListener('scroll', shrinkHeader);
+    };
+  }, []);
 
   return (
     <div ref={headerRef} className="header">
