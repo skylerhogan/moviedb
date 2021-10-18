e8a936ef5eb1794afdc640dcf6edaa8087025651
Skyler Hogan <skylerhogan@skylers-imac.attlocal.net>
2021-10-18 00:49:21 -0500

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/film.svg';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <Link to="/">strmlss</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Terms of Service</Link>
+            <Link to="/">About Us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy Policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Must Watch</Link>
+            <Link to="/">Recent Releases</Link>
+            <Link to="/">Top Rated</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
